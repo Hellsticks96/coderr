d143ad6ce36f602b7df31064c88d3d9da852d519
Leo Albert <leo.albert.96@web.de>
2026-03-25 13:23:41 +0100

    validate review data

--- reviews/api/serializers.py
+++ reviews/api/serializers.py
@@ -17,3 +17,20 @@ class ReviewSerializer(serializers.ModelSerializer):
             "updated_at",
         ]
         read_only_fields = ["business_user", "reviewer", "created_at", "updated_at"]
+
+        def validate(self, data):
+            details = data.get('details')
+    
+            if details is not None:
+                for detail in details:
+                    required_fields = [
+                        "business_user",
+                        "rating",
+                        "description"
+                    ]
+                    for field in required_fields:
+                        if field not in detail:
+                            raise serializers.ValidationError({
+                                'details': f"Field '{field}' is required for each detail."
+                            })
+            return data
