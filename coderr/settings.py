af95455ad171783e0e0edb6bce274fc43e6ec567
Leo Albert <leo.albert.96@web.de>
2026-05-18 15:41:57 +0200

    set empty default for cloudinary env vars since optional

--- coderr/settings.py
+++ coderr/settings.py
@@ -136,9 +136,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 WHITENOISE_ROOT = BASE_DIR / "coderr-frontend"
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": config("CLOUDINARY_API_KEY"),
-    "API_SECRET": config("CLOUDINARY_API_SECRET"),
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME", default=""),
+    "API_KEY": config("CLOUDINARY_API_KEY", default=""),
+    "API_SECRET": config("CLOUDINARY_API_SECRET", default=""),
 }
 
 STORAGES = {
